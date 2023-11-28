f6b97c0926541ca0c13bd8cd3ba1470270260cfd
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-11-28 23:55:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Navbar/Navbar'
+const AboutUs = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h3>This Is About Us Page</h3>
+      
+    </div>
+  )
+}
+
+export default AboutUs
